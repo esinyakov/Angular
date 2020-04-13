2a5be371a83e0b37a3d9e5bb0b0888caca08a76c
esinyakov <evgenysinyakov@icloud.com>
2020-04-13 10:59:27 -0700

    Commit3 Модуль панели администратора

---
+++ blog/src/app/admin/edite-page/edite-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edite-page',
+  templateUrl: './edite-page.component.html',
+  styleUrls: ['./edite-page.component.scss']
+})
+export class EditePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
