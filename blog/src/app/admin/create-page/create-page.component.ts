cc2fc6892b4afc9830560f923455e4565bed3aab
esinyakov <evgenysinyakov@icloud.com>
2020-04-17 04:14:16 -0700

    Commit15 Подключение плагина редактора

--- blog/src/app/admin/create-page/create-page.component.ts
+++ blog/src/app/admin/create-page/create-page.component.ts
@@ -28,10 +28,12 @@ export class CreatePageComponent implements OnInit {
 
     const post: Post = {
       title: this.form.value.title,
-      author: this.form.value.title,
+      author: this.form.value.author,
       text: this.form.value.text,
       date: new Date()
     }
+    
+    console.log(post)
   }
 
 }
